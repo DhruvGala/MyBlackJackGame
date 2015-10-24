c0f1bd79e98e34c7b69b01da7d64d10a103c3262
DhruvGala <dhruv.g123@gmail.com>
2015-10-24 14:42:16 -0400

    card Deck logic and its methods required for the game logic
    shuffling process was implemented efficiently. working on draw card and
    other game logic methods required.

--- MyCards.java
+++ MyCards.java
@@ -4,8 +4,13 @@
  * log: 10-24-2015
  * 
  * Versions: 1.0 basic implementation of a card deck.
- * 			Now working on shuffling process of the cards.
+ * 			 1.1 shuffling process of the cards implemented.
+ * 			Now working on draw card and game logic.
  */
+
+import java.util.Random;
+
+
 
 /**
  * implementation of card deck for my next project a BlackJack Game simulator.
@@ -14,7 +19,7 @@
  *
  */
 public class MyCards {
-	int[][] cardDeck = { { 1, 1 }, { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 }, { 1, 6 }, 
+	static int[][] cardDeck = { { 1, 1 }, { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 }, { 1, 6 }, 
 			{ 1, 7 }, { 1, 8 }, { 1, 9 }, { 1, 10 }, { 1, 11 }, { 1, 12 }, { 1, 13 }, 
 			{ 2, 1 }, { 2, 2 }, { 2, 3 }, { 2, 4 }, { 2, 5 }, { 2, 6 },
 			{ 2, 7 }, { 2, 8 }, { 2, 9 }, { 2, 10 }, { 2, 11 }, { 2, 12 }, { 2, 13 },
@@ -28,43 +33,64 @@ public class MyCards {
 	 * method to display;
 	 * 
 	 */
-	public void display() {
+	public static void display() {
 		
 		for (int i = 0; i < 52; i++) {
 			String card = "";
 			if (cardDeck[i][1] == 1) {
-				card += "Ace";
+				card += "A";
 			} else if (cardDeck[i][1] == 11) {
-				card += "Jack";
+				card += "J";
 			} else if (cardDeck[i][1] == 12) {
-				card += "Queen";
+				card += "Q";
 			} else if (cardDeck[i][1] == 13) {
-				card += "King";
+				card += "K";
 			} else {
 				card += cardDeck[i][1];
 			}
 			
-			card += " of ";
+			//card += " of ";
 
 			if (cardDeck[i][0] == 1) {
-				card += "Clubs ";
+				card += "C ";
 			} else if (cardDeck[i][0] == 2) {
-				card += "Hearts ";
+				card += "H ";
 			} else if (cardDeck[i][0] == 3) {
-				card += "Spades ";
+				card += "S ";
 			} else if (cardDeck[i][0] == 4) {
-				card += "Diamonds ";
+				card += "D ";
 			}
 
-			if((i+1)%13 == 0){
+			/*if((i+1)%13 == 0){
 				System.out.println();
-			}
+			}*/
 			
-			System.out.println(card);
+			System.out.print(card);
 		}
 		
 	}
 	
+	/**
+	 * Method to shuffle the cards.
+	 * 
+	 */
+	public void ShuffleArray()
+	{
+	    int index, temp[][] = new int[1][2];
+	    Random random = new Random();
+	    for (int i = 51; i > 0; i--)
+	    {
+	        index = random.nextInt(i + 1);
+	        temp[0][0] = cardDeck[index][0];
+	        temp[0][1] = cardDeck[index][1];
+	        
+	        cardDeck[index][0] = cardDeck[i][0];
+	        cardDeck[index][1] = cardDeck[i][1];
+	        
+	        cardDeck[i][0] = temp[0][0];
+	        cardDeck[i][1] = temp[0][1];
+	    }
+	}
 	
 	/**
 	 * just for debugging.
@@ -72,6 +98,7 @@ public class MyCards {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new MyCards().display();
+		new MyCards().ShuffleArray();
+		display();
 	}
 }
