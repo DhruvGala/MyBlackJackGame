af9eb9d61998dc6731ff191252df722812745bc1
DhruvGala <dhruv.g123@gmail.com>
2015-10-26 18:43:18 -0400

    initial implementation done
    testing the code with all possible inputs. and refining the code with
    object oriented features.

--- BlackjackGame.java
+++ BlackjackGame.java
@@ -34,9 +34,9 @@ public class BlackjackGame {
 		return usedCards;
 	}
 	
-	public static int calculateScore(int[][] cards,int scoreSoFar){
+	public static int calculateScore(int[][] cards,int scoreSoFar,int start){
 		int score = 0;
-		for(int card=0;card<cards.length;card++){
+		for(int card=start;card<cards.length;card++){
 			if(cards[card][1] == 11 || cards[card][1] == 12 || cards[card][1] == 13){
 				score += 10;
 			}
@@ -56,15 +56,19 @@ public class BlackjackGame {
 		}
 		return score;
 	}
-	
+		
 	public static void letsPlay(){
 		int[][] cards = new int[2][52];
 		cards = deal();
 		
-		//int usedCards[][] = new int[52][2];
-		int pointer = 2;//,cardsAtHand = 2;
-		int score = calculateScore(cards,0);
-		//System.out.println("score: "+score);
+		boolean dealerBurst = false,playerBurst = false;
+		int pointer = 2,dealerScore = 0;
+		int score = calculateScore(cards,0,0);
+		if(score == 21){
+			System.out.println("BLACKJACK!");
+		}
+		
+		System.out.println("score: "+score);
 		
 		int choice = 0;
 		Scanner takeInput = new Scanner(System.in);
@@ -75,25 +79,79 @@ public class BlackjackGame {
 			
 			int[] card = MyCards.drawACard(pointer);
 			MyCards.display(card);
-			//usedCards[pointer][0] = card[0];
-			//usedCards[pointer][1] = card[1];
 			cards[pointer][0] = card[0];
 			cards[pointer][1] = card[1];
 			pointer++;
 			
-			score = calculateScore(cards, score);
+			score = calculateScore(cards, score,0);
 			System.out.println("score: " +score);
 			
 			if(score > 21){
 				System.out.println("Burst!");
+				playerBurst = true;
 				break;
 			}
 			
-			System.out.print("Your choice:\n1.Hit\n2.Stand");
+			
+			System.out.println("Your choice:\n1.Hit\n2.Stand");
 			choice = takeInput.nextInt();
 		}
 		
+		int playerStop = pointer;
 		
+		System.out.println("=======DEALER'S TURN==========");
+		
+		int[] card = MyCards.drawACard(pointer);
+		MyCards.display(card);
+		cards[pointer][0] = card[0];
+		cards[pointer][1] = card[1];
+		pointer++;
+		
+		card = MyCards.drawACard(pointer);
+		MyCards.display(card);
+		cards[pointer][0] = card[0];
+		cards[pointer][1] = card[1];
+		pointer++;
+		
+		dealerScore = calculateScore(cards, dealerScore,playerStop);
+		System.out.println("score: " +dealerScore);
+		
+		while(dealerScore <= 17){
+			card = MyCards.drawACard(pointer);
+			MyCards.display(card);
+			cards[pointer][0] = card[0];
+			cards[pointer][1] = card[1];
+			pointer++;
+			
+			dealerScore = calculateScore(cards, dealerScore,playerStop);
+			System.out.println("score: " +dealerScore);
+			
+			/*if(dealerScore > 17){
+				System.out.println("Dealer Stands!");
+				break;
+			}*/
+		}
+		
+		if(dealerScore > 21){
+			System.out.println("Dealer is Burst!");
+			dealerBurst = true;
+		}
+		
+		if(dealerBurst && playerBurst || (score == dealerScore)){
+			System.out.println("ITS A DRAW!");
+		}
+		
+		else if(dealerBurst && !playerBurst){
+			System.out.println("Player Wins!");
+		}
+		
+		else if(dealerScore > score){
+			System.out.println("Dealer Wins!");
+		}
+		
+		else if (score > dealerScore) {
+			System.out.println("Player Wins!");
+		}
 		
 		
 		MyCards.cardDeck = MyCards.pushBackUsedCards(MyCards.cardDeck, pointer, cards);
@@ -104,8 +162,6 @@ public class BlackjackGame {
 	
 	public static void main(String[] args) {
 		newDeck.ShuffleArray();
-		//newDeck.display();
-		//deal();
 		letsPlay();
 	}
 			
